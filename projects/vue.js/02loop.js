d3ec9052d7e9dd253b2fc1b5242d7cee8567c911
Steve Albers <stevealbers@hotmail.com>
2018-08-17 02:38:30 -0400

    Moving real talk over to GitHub repo

---
+++ projects/vue.js/02loop.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+      items: [
+        'Virginia',
+        'Maryland',
+        'District of Columbia',
+        'Pennsylvania',
+        'North Carolina',
+        'West Virginia',
+      ]
+    }
+  });
---
+++ projects/vue.js/databinding.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            person: {
+                name: 'Steve',
+                age: 'old',
+            }
+         }
+    }
+  });
